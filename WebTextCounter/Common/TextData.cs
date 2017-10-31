f9c2753970f07b0aa38fe14208f46b0cafc55351
Vinicios de Moura <vhmoura@gmail.com>
2017-10-31 19:25:11 -0200

    Modified for simplicity

---
+++ WebTextCounter/Common/TextData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTextCounter.Common
+{
+    public class TextData
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public bool IsPrime { get; set; }
+    }
+
+}
--- WebTextCounter/Service/WebTextCounterService.cs
+++ WebTextCounter/Service/WebTextCounterService.cs
@@ -5,17 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using WebTextCounter.Common;
 using WebTextCounter.Helpers;
 
 namespace WebTextCounter.Service
-{
-    public class AA
-    {
-        public string Name { get; set; }
-        public int Count { get; set; }
-        public bool IsPrime { get; set; }
-    }
-
+{    
     public class WebTextCounterService
     {
         private string webAddress;
@@ -25,30 +19,30 @@ namespace WebTextCounter.Service
             this.webAddress = address;
         }
 
-        public List<AA> GetData()
+        public List<TextData> GetData()
         {
             if (string.IsNullOrWhiteSpace(webAddress))
                 throw new Exception("Invalid web address");
 
             try
             {
-                var task = Task.FromResult<string>(WebHelper.GetWebData(webAddress))
-                .ContinueWith(i =>
-                {
-                    var reg = new Regex("[^a-zA-Z']");
-                    return i.Result.Split(new char[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries) // remove new line
-                                       .Select(s => reg.Replace(s, string.Empty).ToLower()) // take only chars
-                                       .GroupBy(a => a) // group and count
-                                       .Select(m => new AA { Name = m.Key, Count = m.Count(), IsPrime = MathHelper.IsPrime(m.Count()) })
-                                       .ToList();
-                });
-
-                return task.Result;
+                var webData = WebHelper.GetWebData(webAddress);                
+                return _GetTextDataFromWebResult(webData);
             }
             catch
             {
                 throw new Exception("Invalid web address");
             }                           
+        }
+
+        private List<TextData> _GetTextDataFromWebResult(string webData)
+        {            
+            var reg = new Regex("[^a-zA-Z']");
+            return webData.Split(new char[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries) // remove new line
+                                       .Select(s => reg.Replace(s, string.Empty).ToLower()) // take only chars
+                                       .GroupBy(a => a) // group, count and check for prime
+                                       .Select(m => new TextData { Name = m.Key, Count = m.Count(), IsPrime = MathHelper.IsPrime(m.Count()) })
+                                       .ToList();
         }
     }
 }
